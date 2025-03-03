35077290b9a21172b594d6a199287742c28e85e3
sylv256 <contact@sylv.gay>
2025-03-02 21:07:35 -0500

    feat: Blur images and disable embeds in NSFW channels

---
+++ src/plugins/nsfw-blur/manifest.js
@@ -0,0 +1,15 @@
+/** @type {import("@vtypes/plugin").PluginManifest} */
+export default {
+    name: "NSFW Blur",
+    description: `Blur image previews in NSFW channels.`,
+    authors: [
+        {
+            name: "sylv256",
+            id: "209015289990348800"
+        }
+    ],
+    main: "src/index.ts",
+    vendetta: {
+        icon: "ic_image"
+    }
+}
---
+++ src/plugins/nsfw-blur/src/index.ts
@@ -0,0 +1,59 @@
+import { logger } from '@vendetta'
+import { findByName } from '@vendetta/metro'
+import { ReactNative } from '@vendetta/metro/common'
+import { before } from '@vendetta/patcher'
+import { definePlugin } from '@vtypes/plugin'
+import Settings from './Settings'
+
+let patches: Array<Function> = []
+
+export default definePlugin({
+	onLoad() {
+		const transformMessageAttachments = findByName('transformMessageAttachments', false)
+		const createMessageContent = findByName('createMessageContent', false)
+		const getChannel = findByProps('getChannel').getChannel
+
+		patches.push(
+			before('default', transformMessageAttachments, args => {
+				return
+				let message = args[0]
+				// retain sanity
+				if (!message || !(message.attachments instanceof Array)) return
+
+				message.shouldObscureSpoiler = true
+				for (const attachment of message.attachments) {
+					attachment.spoiler = true
+				}
+			})
+		)
+
+		patches.push(
+			before('default', createMessageContent, args => {
+				let content = args[0]
+				if (!content || typeof content !== 'object') return
+				if (!content.message) return
+				if (!content.message.channel_id || !getChannel(content.message.channel_id).nsfw_) return
+				if (!content.options) return
+				content.options.inlineEmbedMedia = false
+				let message = content.message
+				// retain sanity
+				if (!message || !(message.attachments instanceof Array)) return
+
+				content.options.shouldObscureSpoiler = true
+				for (const attachment of message.attachments) {
+					attachment.spoiler = true
+				}
+				logger.info(content)
+			})
+		)
+
+		// for future reference
+		//if (m.content) m.content = m.content
+		//	.replace(/(http[s?]:\/\/(.+)\.(gif|mp4|png|jpg|jpeg|jxl))/g, (_, s) => `||${s}||`)
+	},
+	onUnload() {
+		for (const unpatch of patches) {
+			unpatch()
+		}
+	}
+});
